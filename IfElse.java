1d3552c04954a25c93c9881ac7b00ab443488bf2
LyaSantos24 <147869869+LyaSantos24@users.noreply.github.com>
2024-10-04 18:47:26 -0400

    IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 66;
+
+        if (idade <=10) {
+
+            System.out.println("criança");
+
+        }else if (idade <= 17) {
+            System.out.println("adolescente");
+
+        } else if (idade <= 65) {
+            System.out.println("adulto");
+
+        } else {
+            System.out.println("idoso");
+        }
+
+   }
+
+}
+
+
+
